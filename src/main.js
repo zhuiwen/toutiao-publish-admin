85d1ecf4f6ba2987afa3ffeffd235076cce6d495
zhuiwen <1227287697@qq.com>
2021-02-02 13:43:27 +0800

    调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
+/**
+ * 项目启动入口
+ */
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 加载全局样式
+import './styles/index.less'
 Vue.config.productionTip = false
-
+// 创建vue根实例
+// 把router配置到根实例
+// 通过render方法把App根组件渲染到#app入口节点
 new Vue({
   router,
   render: h => h(App)
+  // el: '#app' 等价于 $mount('#app')
 }).$mount('#app')
